2d50641be984a043bd6e18656686f6f44fd84f1b
Jessicawettemann <115081083+Jessicawettemann@users.noreply.github.com>
2024-04-24 15:54:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,18 @@ import requests
 import os
 
 API_URL = "https://chat.openai.com/g/g-wn8pp4t4P-grafikdesigner/predict"
-API_KEY = os.getenv('OPENAI_API_KEY')  
+
+import os
+
+# Abrufen des API-Schlüssels aus den Umgebungsvariablen
+API_KEY = os.getenv("API_KEY")
+
+# Überprüfen, ob der API-Schlüssel korrekt abgerufen wurde
+if not API_KEY:
+    raise ValueError("API_KEY ist nicht in den Umgebungsvariablen definiert.")
+
+# Verwenden Sie API_KEY in Ihrer Anwendung
+print("Mein API-Schlüssel ist:", API_KEY)
 def get_model_response(text):
     headers = {
         "Authorization": f"Bearer {API_KEY}"
