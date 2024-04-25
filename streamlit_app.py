4d480b875475c17623c6e9f1668e22ddb6f463ed
Jessicawettemann <115081083+Jessicawettemann@users.noreply.github.com>
2024-04-25 10:43:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,29 @@
-import openai
 from openai import OpenAI
-import os
+import streamlit as st
 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-API_URL = "https://chat.openai.com/g/g-wn8pp4t4P-grafikdesigner/predict"
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-
-
-client = OpenAI()
-# Überprüfen, ob der API-Schlüssel korrekt abgerufen wurde
-if not api_key:
-    raise ValueError("API_KEY ist nicht in den Umgebungsvariablen definiert.")
-
-# Verwenden Sie API_KEY in Ihrer Anwendung
-print("Mein API-Schlüssel ist:", api_key)
-def get_model_response(text):
-    headers = {
-        "Authorization": f"Bearer {api_key}"
-    }
-    try:
-        response = requests.post(API_URL, json={"text": text}, headers=headers)
-        if response.status_code == 200:
-            return response.json()['response']
-        else:
-            return f"Fehler: API antwortete mit Statuscode {response.status_code}"
-    except Exception as e:
-        return f"Ausnahme aufgetreten: {str(e)}"
-
-st.title('Mein Custom GPT Interface')
-
-user_input = st.text_area("Text eingeben")
-if st.button('Antwort erhalten'):
-    response = get_model_response(user_input)
-    st.text_area("Antwort", response, height=300)
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
